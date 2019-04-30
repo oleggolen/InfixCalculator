de80d1b9482446c68d90a16bcc37da8ced833ac8
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2019-04-30 18:41:42 +0700

    cos added

---
+++ InfixCalculator/Operations/Cos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InfixCalculator.Operands;
+
+namespace InfixCalculator.Operations
+{
+    public class Cos : IOperation
+    {
+        public int Priority => 2;
+
+        public int OperandsCount => 1;
+
+        public IList<IOperand> Operands { get; private set; }
+        public Cos()
+        {
+            Operands = new List<IOperand>();
+        }
+
+        public IOperand Execute()
+        {
+            if (Operands == null || !Operands.Any()) throw new InvalidOperationException("Нельзя выполнить операцию без операндов");
+            if (Operands[0] == null) throw new InvalidOperationException("Один из операндов пуст или не существует");
+            return new Operand((float)Math.Cos(Operands[0].Value));
+        }
+    }
+}
